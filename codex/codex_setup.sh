abfa4db046659b2ac614f806ffe1d263f132c914
Curtis Hendrickson <curtish@uab.edu>
2026-02-11 18:11:29 -0600

    map git to use https, in place of ssh, in codex environment

--- codex/codex_setup.sh
+++ codex/codex_setup.sh
@@ -11,5 +11,9 @@ pip install -U pip
 pip install -r pip-requirements.txt
 
 # clone submodule repos
+# work around ssh block in codex env - map to https instead
+git config --global url."https://github.com/".insteadOf "git@github.com:"
+git config --global url."https://github.com/".insteadOf "ssh://git@github.com/"
+# do the actual submodule pull
 git submodule sync --recursive
 git submodule update --init --recursive
