043ae89b8137924ad0ba70eb298fba878f83d702
Curtis Hendrickson <curtish@uab.edu>
2026-03-20 16:22:02 -0500

    VMR_MSL41.v1_20260320 export

---
+++ VMR_MSL41v1_20260320/run_export.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Export VMR.xlsx for VMR_VMR41v1_20260320 (date MSL41.v1 data hit prod website)
+#
+# exports from ICTVdatabase/data/ directory
+#
+DATA_SOURCE="./MSL41v1/data"
+DATA_MASK="../db_mask.tsv"
+TEMPLATE="template-VMR_MSL41.v1.20260320.editor.xlsx"
+OUTPUT="VMR_MSL41.v1.20260320.editor.xlsx"
+
+cat <<EOF
+#
+# run export from '$DATA_SOURCE'
+#
+../vmr_export.py --keep-going --verbose --data_source "$DATA_SOURCE" --mask "$DATA_MASK" --template "$TEMPLATE" --output "$OUTPUT"
+EOF
+../vmr_export.py --keep-going --verbose --data_source "$DATA_SOURCE" --mask "$DATA_MASK" --template "$TEMPLATE" --output "$OUTPUT"
+
+if [[ $? -ne 0 ]]; then
+    echo "FAIL"
+else
+    echo "SUCCESS: $OUTPUT"
+fi
